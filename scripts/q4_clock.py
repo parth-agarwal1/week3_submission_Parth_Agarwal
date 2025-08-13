21c1cf2803d681c5b0ec6132efcad927f7d9a15c
parth-agarwal1 <parthagarwal212@gmail.com>
2025-08-13 16:53:09 +0530

    Update q4_clock.py

--- scripts/q4_clock.py
+++ scripts/q4_clock.py
@@ -12,8 +12,8 @@ class Clock(Node):
     def __init__(self):
         super().__init__('clock')
         self.sec_pub = self.create_publisher(Int32, '/second', 10)
-        self.min_pub = self.create_publisher(Int32, '/second', 10)
-        self.hour_pub = self.create_publisher(Int32, '/second', 10)
+        self.min_pub = self.create_publisher(Int32, '/minute', 10)
+        self.hour_pub = self.create_publisher(Int32, '/hour', 10)
         self.time_pub = self.create_publisher(String, '/clock', 10)
         self.create_publisher(Int32, '/second', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
